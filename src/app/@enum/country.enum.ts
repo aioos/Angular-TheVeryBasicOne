16c6f1af3e9896c880386d60a93e6769ec017f49
aioos <ar.fs.yt@gmail.com>
2021-10-11 13:10:42 +0800

    新增国家

--- src/app/@enum/country.enum.ts
+++ src/app/@enum/country.enum.ts
@@ -5,5 +5,12 @@ export enum Country {
   cl = 'cl',
   mx = 'mx',
   ph = 'ph',
+  za = 'za',
+  au = 'au',
+  pr = 'pr',
+  ca = 'ca',
+  ng = 'ng',
+  fr = 'fr',
+  de = 'de',
   others = 'others'
 }
